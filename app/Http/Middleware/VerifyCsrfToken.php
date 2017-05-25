a71fc95221d9d94958f0dfa575423d5687038147
lyx <44600937@qq.com>
2017-05-25 20:26:10 +0800

    debug

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Illuminate\Session\TokenMismatchException;
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -14,4 +16,25 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param Closure $next
+     * @return mixed
+     * @throws TokenMismatchException
+     */
+    public function handle($request, Closure $next)
+    {
+        // Add this:
+        if ($request->method() == 'POST') {
+            return $next($request);
+        }
+
+        if ($request->method() == 'GET' || $this->tokensMatch($request)) {
+            return $next($request);
+        }
+        throw new TokenMismatchException();
+    }
 }
