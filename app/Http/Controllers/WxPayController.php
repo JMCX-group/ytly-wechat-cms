74c905ed13813434f4d571ddb417d6800f7cd96b
lyx <44600937@qq.com>
2017-05-25 21:23:52 +0800

    debug

--- app/Http/Controllers/WxPayController.php
+++ app/Http/Controllers/WxPayController.php
@@ -29,28 +29,24 @@ class WxPayController extends Controller
             if($productId == '20170525'){
                 $prepayId = EasyWeChat::newNativeOrder('201705252105', $openId, $productId);
                 Log::info('payNotifyUrl-order', ['context' => $prepayId]);
+
+                /**
+                 * 参数组:
+                 */
+                $data = array(
+                    'return_code' => 'SUCCESS',
+                    'appid' => env('WECHAT_APPID'), // AppID
+                    'mch_id' => env('WECHAT_MCH_ID'),
+                    'nonce_str' => $notify->nonce_str,
+                    'prepay_id' => $prepayId,
+                    'result_code' => 'SUCCESS',
+                );
+                $data['sign'] = $this->wxMd5Sign($data);
+                $dataXml = $this->wxArrayToXml($data);
             }
 
-//            // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
-//            $order = 查询订单($notify->out_trade_no);
-//            if (!$order) { // 如果订单不存在
-//                return 'Order not exist.'; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
-//            }
-//            // 如果订单存在
-//            // 检查订单是否已经更新过支付状态
-//            if ($order->paid_at) { // 假设订单字段“支付时间”不为空代表已经支付
-//                return true; // 已经支付成功了就不再更新了
-//            }
-//            // 用户是否支付成功
-//            if ($successful) {
-//                // 不是已经支付状态则修改为已经支付状态
-//                $order->paid_at = time(); // 更新支付时间为当前时间
-//                $order->status = 'paid';
-//            } else { // 用户支付失败
-//                $order->status = 'paid_fail';
-//            }
-//            $order->save(); // 保存订单
-            return true; // 返回处理完成
+
+            return $dataXml; // 返回处理完成
         });
 //
         return $response;
@@ -152,5 +148,53 @@ class WxPayController extends Controller
         } catch (\Exception $e) {
             die($e->getMessage());
         }
+    }
+
+    /**
+     * Array to xml.
+     *
+     * @param null $parameters
+     * @return string
+     */
+    public function wxArrayToXml($parameters = NULL)
+    {
+        if (is_null($parameters)) {
+            $parameters = $this->parameters;
+        }
+
+        if (!is_array($parameters) || empty($parameters)) {
+            die('参数不为数组无法解析');
+        }
+
+        $xml = "<xml>";
+        foreach ($parameters as $key => $val) {
+            $xml .= "<" . $key . "><![CDATA[" . $val . "]]></" . $key . ">";
+        }
+        $xml .= "</xml>";
+
+        return $xml;
+    }
+
+    /**
+     * Random.
+     *
+     * @param $length
+     * @param int $numeric
+     * @return string
+     */
+    public function random($length, $numeric = 0)
+    {
+        if ($numeric) {
+            $hash = sprintf('%0' . $length . 'd', mt_rand(0, pow(10, $length) - 1));
+        } else {
+            $hash = '';
+            $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
+            $max = strlen($chars) - 1;
+            for ($i = 0; $i < $length; $i++) {
+                $hash .= $chars[mt_rand(0, $max)];
+            }
+        }
+
+        return $hash;
     }
 }
