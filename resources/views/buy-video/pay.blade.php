fb2f8cbc1287f2bcefec90ede8d2cfa59a19e9b2
liyingxuan <mvp_xuan@163.com>
2017-10-27 18:04:54 +0800

    debug

--- resources/views/buy-video/pay.blade.php
+++ resources/views/buy-video/pay.blade.php
@@ -11,9 +11,9 @@
 @extends('layouts.main-info')
 
 @section('content')
-    <div>
+    <div class="div-centered">
+        <button onclick="callpay()" style="width:210px; height:50px;font-size:16px;" type="button">立即支付</button>
     </div>
-    <button onclick="callpay()" style="width:210px; height:50px;font-size:16px;" type="button">立即支付</button>
 @stop
 
 
@@ -46,4 +46,19 @@
             }
         }
     </script>
+@stop
+
+@section('style')
+    <style>
+        /* DIV居中 */
+        .div-centered {
+            width: 300px;
+            height: 200px;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            margin: -100px 0 0 -150px;
+            font-size: 30px;
+        }
+    </style>
 @stop
