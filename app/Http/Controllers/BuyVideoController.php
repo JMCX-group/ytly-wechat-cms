825270427f202d158bcfc85cb510b6f8e79a4471
liyingxuan <mvp_xuan@163.com>
2017-10-27 17:41:36 +0800

    debug

--- app/Http/Controllers/BuyVideoController.php
+++ app/Http/Controllers/BuyVideoController.php
@@ -74,8 +74,6 @@ class BuyVideoController extends Controller
     public function store(Request $request)
     {
         $user_info = $this->getUserInfo();
-
-        dd($user_info);
         $phone = $request['phone'];
 
         /**
